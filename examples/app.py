e2ebe8e98283b1ceaebc2ba72e348f52ced55077
Elysson MR <elyssonmr@gmail.com>
2025-06-30 18:52:31 -0300

    Add websocket_article implementation

--- examples/app.py
+++ examples/app.py
@@ -1,9 +1,11 @@
 from fastapi.applications import FastAPI
 
 from examples.file_upload.routes import router as file_upload_router
+from examples.websocket.routes import router as websocket_router
 
 app = FastAPI(
     description='FastAPI Examples',
 )
 
 app.include_router(file_upload_router)
+app.include_router(websocket_router)
